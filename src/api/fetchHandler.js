762e897fe21e744228f2f77ef9f6e31cd8af1255
RJ Salvador <randolph.salvador@gmail.com>
2019-03-15 13:18:33 -0400

    Did pair coding with Tom and Connor

--- src/api/fetchHandler.js
+++ src/api/fetchHandler.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 class FetchHandler {
-  static getImage(queryParam = 'thingy', reqTimestamp) {
+  static getImage(queryParam = "thingy", reqTimestamp) {
     return this.get("https://imagesearch.dev.tophat.com/", {
       query: queryParam,
-      timestamp: reqTimestamp,
-    })
-    .then(imgData => {
+      timestamp: reqTimestamp
+    }).then(imgData => {
       return imgData;
-    })
+    });
   }
   static get(endpoint, parameters) {
     return axios.get(endpoint, { params: parameters });
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <input type="text" id="searchString" name="searchString" v-model="searchString" v-on:input="searchChange" />
+    <input
+      type="text"
+      id="searchString"
+      name="searchString"
+      v-model="searchString"
+      v-on:input="searchChange"
+    />
     <p>
       This makes an API call to https://jsonplaceholder.typicode.com/posts and
       displays the results.
     </p>
     <hr />
-    <img v-bind:src="image_url"/>
+    <img v-bind:src="image_url" />
   </div>
 </template>
 
@@ -22,9 +28,9 @@ export default {
   data() {
     return {
       isLoading: true,
-      image_url: '',
-      searchString: 'init',
-      mostRecentRequestTime: 0,
+      image_url: "",
+      searchString: "init",
+      mostRecentRequestTime: 0
     };
   },
   created() {
@@ -44,21 +50,25 @@ export default {
     },
     doSearch: function(searchString, requestTimestamp) {
       FetchHandler.getImage(searchString, requestTimestamp)
-      .then(imgData => {
-        const timestampIdx = imgData.request.responseURL.search(/timestamp=\d+/);
-        const timestamp = imgData.request.responseURL.slice(timestampIdx + 10);
+        .then(imgData => {
+          const timestampIdx = imgData.request.responseURL.search(
+            /timestamp=\d+/
+          );
+          const timestamp = imgData.request.responseURL.slice(
+            timestampIdx + 10
+          );
 
-        if(timestamp >= this.mostRecentRequestTime) {
-          this.image_url = imgData.data.image_url;
-        }
-      })
-      .catch(err => {
-        console.log(JSON.stringify(err, null, 2));
-        this.doSearch(searchString, requestTimestamp);
-      });
+          if (timestamp >= this.mostRecentRequestTime) {
+            this.image_url = imgData.data.image_url;
+          }
+        })
+        .catch(err => {
+          console.log(JSON.stringify(err, null, 2));
+          this.doSearch(searchString, requestTimestamp);
+        });
     },
     testy: function(param) {
-      console.log('yep!');
+      console.log("yep!");
     }
   }
 };
